f2daeb370d8e0b34e9da30e0967f2c09576838a9
Nitin Khare <nknitinkhare13@gmail.com>
2020-09-13 15:27:36 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const express= require('express');
 const bodyParser= require('body-parser');
+
+
+const app=express();
+const PORT = process.env.PORT || 3000
+app.use(bodyParser.urlencoded({extended:true}));
+
+//route handlers
 const adminroutes=require('./routes/admin');
 const shoproutes=require('./routes/shop');
 
-const app=express();
-app.use(bodyParser.urlencoded({extended:true}));
+//route middlewares
 app.use('/admin',adminroutes);
 app.use(shoproutes);
+//route middleware end
 
-// app.use((req,res,next)=>{
-//     console.log("In the middleware");
-//     next();//allows the request to continue to next middleware in line
-// });
-app.use((req,res,next)=>{
-    res.status(404).send("<h1>Page not found!!</h1>")
-});
 
-app.listen(3000);
+app.listen(PORT);
